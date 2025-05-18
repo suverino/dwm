febb38d32bd23d2dd4a1f90d8fb0e958443b3ab6
suverino <suverino.frith@gmail.com>
2025-05-18 10:42:35 -0400

    add single window gap and also auto window sorting

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const unsigned int gappih    = 10;       /* horiz inner gap between windo
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;      /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=11" };
@@ -40,8 +40,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       0,						0,           -1 },
+	{ "obsidian", NULL,       NULL,       1 << 5,				0,           -1 },
+	{ "Slack",    NULL,       NULL,       1 << 8,				0,           -1 },
+	{ "St",       NULL,       "genius",   0,				    1,           -1 },
 };
 
 /* layout(s) */
