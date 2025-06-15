83d60ede32a83d290f055f3def42f5584b5082b5
suverino <suverino.frith@gmail.com>
2025-06-15 10:16:04 -0400

    add dwm swallow patch

--- config.h
+++ config.h
@@ -3,10 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;      /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -39,10 +40,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "obsidian", NULL,       NULL,       1 << 5,				0,           -1 },
-	{ "Slack",    NULL,       NULL,       1 << 8,				0,           -1 },
-	{ "St",       NULL,       "genius",   0,				    1,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "obsidian", NULL,    NULL,           1 << 5,    0, 	      0,           0,        -1 },
+	{ "Slack",   NULL,     NULL, 	       1 << 8,    0,          0,           0,        -1 },
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "St",      NULL,  "genius",          0,         1,          1,           0,        -1 },
 };
 
 /* layout(s) */
